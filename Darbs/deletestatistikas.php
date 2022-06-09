e441c1814cad35ef6d175ea94b1ba7c2ad2ffdf8
HLasinskis <56246570+HLasinskis@users.noreply.github.com>
2022-06-09 06:39:19 +0300

    Delete Darbs directory

--- Darbs/deletestatistikas.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('./config/config.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM statistika WHERE id=$id"; 
-$result = mysqli_query($conn,$query) or die ( mysqli_error());
-header("Location: viewstatistika.php"); 
-?>
